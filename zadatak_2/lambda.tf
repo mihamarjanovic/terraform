d59f101bc0b62166af1dcac9a6c5187f002a6ba3
mihamarjanovic <marjanovic.galaxy@gmail.com>
2025-09-01 14:49:21 +0200

    Adding logging

--- zadatak_2/lambda.tf
+++ zadatak_2/lambda.tf
@@ -2,7 +2,7 @@ resource "aws_lambda_function" "hello_world" {
   filename         = data.archive_file.hello_world.output_path
   function_name    = "${local.project}_lambda_function"
   role             = aws_iam_role.hello_world_role.arn
-  handler          = "hello_world.lambda_handler"
+  handler          = "hello_world.handler"
   source_code_hash = data.archive_file.hello_world.output_base64sha256
 
   runtime = "python3.12"
